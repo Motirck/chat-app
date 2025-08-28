4c8687f25508114aa3ff3fa1122700cd5da986ad
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2025-08-28 16:14:57 -0300

    Bugfix: Fixing interface

--- src/ChatApp.Core/Interfaces/IChatRepository.cs
+++ src/ChatApp.Core/Interfaces/IChatRepository.cs
@@ -4,14 +4,10 @@ namespace ChatApp.Core.Interfaces;
 
 public interface IChatRepository
 {
-    public interface IChatRepository
-    {
-        Task<IEnumerable<ChatMessage>> GetLastMessagesAsync(int count = 50);
-        Task<ChatMessage> AddMessageAsync(ChatMessage message);
-        Task<User?> GetUserByUsernameAsync(string username);
-        Task<User?> GetUserByIdAsync(int id);
-        Task<User> CreateUserAsync(User user);
-        Task<bool> ValidateUserCredentialsAsync(string username, string passwordHash);
-    }
-
+    Task<IEnumerable<ChatMessage>> GetLastMessagesAsync(int count = 50);
+    Task<ChatMessage> AddMessageAsync(ChatMessage message);
+    Task<User?> GetUserByUsernameAsync(string username);
+    Task<User?> GetUserByIdAsync(int id);
+    Task<User> CreateUserAsync(User user);
+    Task<bool> ValidateUserCredentialsAsync(string username, string passwordHash);
 }
