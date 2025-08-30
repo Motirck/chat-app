cfd0c6eaa7fcd0c61dcde174f78c2bb25f3a6a91
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2025-08-30 10:41:23 -0300

    Feat: Adding validators using Fluent Validation

---
+++ src/ChatApp.Core/Dtos/Validators/StockQuoteDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace ChatApp.Core.Dtos.Validators;
+
+public class StockQuoteDtoValidator : AbstractValidator<StockQuoteDto>
+{
+    public StockQuoteDtoValidator()
+    {
+        RuleFor(x => x.StockCode)
+            .NotEmpty()
+            .MaximumLength(10)
+            .Matches("^[a-zA-Z.]+$");
+
+        RuleFor(x => x.Username)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.Quote)
+            .NotEmpty()
+            .MaximumLength(2000);
+
+        RuleFor(x => x.Timestamp)
+            .NotEqual(default(DateTime));
+    }
+}
---
+++ src/ChatApp.Core/Entities/Validators/ApplicationUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace ChatApp.Core.Entities.Validators;
+
+public class ApplicationUserValidator : AbstractValidator<ApplicationUser>
+{
+    public ApplicationUserValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotEmpty();
+
+        RuleFor(x => x.Email)
+            .EmailAddress().When(x => !string.IsNullOrWhiteSpace(x.Email));
+
+        RuleFor(x => x.CreatedAt)
+            .NotEqual(default(DateTime));
+
+        RuleFor(x => x.LastLoginAt)
+            .NotEqual(default(DateTime)).When(x => x.LastLoginAt != default);
+    }
+}
