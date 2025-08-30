2f83926c91e87d2b3ed7c799ecc1f98b4c5d25a3
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2025-08-30 11:07:17 -0300

    Feat: Adding validator

---
+++ src/ChatApp.Core/Dtos/Validators/StockCommandDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace ChatApp.Core.Dtos.Validators;
+
+public class StockCommandDtoValidator : AbstractValidator<StockCommandDto>
+{
+    public StockCommandDtoValidator()
+    {
+        RuleFor(x => x.StockCode)
+            .NotEmpty().WithMessage("Stock code is required")
+            .MaximumLength(10)
+            .Matches("^[a-zA-Z.]+$").WithMessage("Stock code must contain letters and optional dot");
+
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Username is required")
+            .MaximumLength(50);
+    }
+}
