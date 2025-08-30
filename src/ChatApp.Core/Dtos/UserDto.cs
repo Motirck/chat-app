a3b74ab876a665808a8aec6e43d393a7ce41c800
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2025-08-30 10:40:24 -0300

    Feat: Removed user created before using Identity

--- src/ChatApp.Core/Dtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChatApp.Core.Dtos;
-
-public record UserDto
-{
-    public int Id { get; init; }
-    public string Username { get; init; } = string.Empty;
-    public DateTime CreatedAt { get; init; }
-    public bool IsActive { get; init; }
-}
